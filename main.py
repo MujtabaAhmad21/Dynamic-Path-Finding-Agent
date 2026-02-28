edfcdb6ae02747a69ea8ede4d8e7889aae6facd2
Mujtaba Ahmad <mma212005@gmail.com>
2026-02-28 15:49:03 +0500

    added A* algorithm
    didn't add path that trace back to start from goal node. in next commit will do so

--- main.py
+++ main.py
@@ -82,7 +82,18 @@ class Node:
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
     def update_neighbors(self, grid):
-        pass
+        self.neighbors = []
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():  # DOWN
+            self.neighbors.append(grid[self.row + 1][self.col])
+
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():  # UP
+            self.neighbors.append(grid[self.row - 1][self.col])
+
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():  # RIGHT
+            self.neighbors.append(grid[self.row][self.col + 1])
+
+        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():  # LEFT
+            self.neighbors.append(grid[self.row][self.col - 1])
 
     # lt means less than and it is being used to compare two nodes
     def __lt__(self, other):
@@ -93,6 +104,50 @@ def hueristic(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
+
+def algorithm(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    # tracking path
+    came_from = {}
+    g_score = {node: float('inf') for row in grid for node in row}
+    g_score[start] = 0
+    f_score = {node: float('inf') for row in grid for node in row}
+    f_score[start] = hueristic(start.get_pos(), end.get_pos())
+
+    open_set_hash = {start}
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+
+        if current == end:
+            return True
+        
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1
+
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current 
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + hueristic(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
+
+        draw()
+        
+        if current != start:
+            current.make_closed()
+
+    return False
 
 def make_grid(rows, width):
     grid = []
@@ -180,6 +235,14 @@ def main(win, width):
                     start = None
                 elif node == end:
                     end = None
+
+            if event.type == pygame.KEYDOWN:
+                if event == pygame.K_SPACE and not started:
+                    for row in grid:
+                        for node in row:
+                            node.update_neighbors(grid)
+
+                    algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
 
     pygame.quit()
 
